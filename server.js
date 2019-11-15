c59476b749d8628ebfaeca4abc91e41c1e236143
Ha Nguyen <hanguyen.0096@gmail.com>
2019-11-14 21:58:15 -0500

    add api-token

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 
@@ -5,9 +6,21 @@ const app = express()
 console.log(process.env.API_TOKEN)
 app.use(morgan('dev'))
 
-app.use((req, res) => {
-  res.send('Hello, world!')
+app.use(function validateBearerToken(req, res, next) {
+  console.log('validate bearer token middleware')
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  next()
 })
+
+function handleGetMovie(req, res) {
+  res.send('Hello, welcome to moviedex!')
+}
+    
+app.get('/movie', handleGetMovie)
 
 const PORT = 8000
 
